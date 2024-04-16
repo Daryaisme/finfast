3fc64fc46bcb5a09e9a3c36fd88be74ec273e7d8
Darya Suravtsova <darblnkapm@gmail.com>
2024-04-16 16:18:23 +0300

    feat: add burger-menu navigation

--- assets/scripts/burgerMenu.js
+++ assets/scripts/burgerMenu.js
@@ -1,8 +1,29 @@
+// const REALESTATE = 'real-estate';
+// const FINANCE = 'finance';
+// const BUSINESS = 'business';
+// const NATIONALITY = 'nationality';
+
 const headerMenu = document.querySelectorAll('.header__menu');
 for (let menu of headerMenu) menu.addEventListener('click', handleHeaderMenuClick);
 
 const burgerMenu = document.querySelector('.burger-menu');
 
+const burgerMenuNavItems = document.querySelectorAll('.burger-menu__nav-item');
+burgerMenuNavItems.forEach(item => item.addEventListener('click', handleNavItemClick));
+
+const burgerMenuNavContent = document.querySelectorAll('.burger-menu__nav-content');
+
 function handleHeaderMenuClick() {
   document.documentElement.style.overflowY = burgerMenu.classList.toggle('burger-menu_open') ? 'hidden' : 'visible';
+}
+
+function handleNavItemClick(e) {
+  const item = e.currentTarget;
+  const itemType = item.dataset.navItemType;
+
+  burgerMenuNavItems.forEach(item => item.classList.remove('burger-menu__nav-item_active'));
+  item.classList.add('burger-menu__nav-item_active');
+
+  burgerMenuNavContent.forEach(content => content.classList.remove('burger-menu__nav-content_active'));
+  burgerMenuNavContent.forEach(content => {if (content.dataset.navItemType === itemType) content.classList.add('burger-menu__nav-content_active')});
 }
